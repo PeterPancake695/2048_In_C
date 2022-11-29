c3c48c6cbbd2ab4e224d9e28e49a8ddeb73c4795
PeterPancake666 <96543374+PeterPancake666@users.noreply.github.com>
2022-11-29 21:34:07 +0200

    Created window and finished resources.h

--- src/init.h
+++ src/init.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "game.h"
+#include "resources.h"
 
 void init(){
-    InitWindow(720, 720, "2048_In_C");
-    
+    srand(time(NULL));
+    resource_all_load();
+    InitWindow(window_width, window_height, window_title);
+    SetTargetFPS(60);
+    SetWindowIcon(resources.textures.misc.icon);
+    while (!WindowShouldClose()){
+        BeginDrawing();
+            ClearBackground((Color){244, 226, 198, 255});
+        EndDrawing();
+    }
+    CloseWindow();
+    resource_all_unload();
 }
