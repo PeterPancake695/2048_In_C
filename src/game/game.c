bdc47208ee85cd4d64da5f1ade6a463103755906
PeterPancake666 <96543374+PeterPancake666@users.noreply.github.com>
2022-12-30 17:19:49 +0200

    Added pause menu

--- src/game/game.c
+++ src/game/game.c
@@ -28,6 +28,13 @@ void initGame(){
 			gui_game_pause_draw();
 		}
 		gui_game_pause_input();
+		gui_game_pause_button();
+		if(game_pressed_pause == true){
+			gamePaused--;
+			printf("pressed pause button\n");
+			game_pressed_pause_hold = false;
+			game_pressed_pause = false;
+		}
 	}
     LoseCheck();
     WinCheck();
